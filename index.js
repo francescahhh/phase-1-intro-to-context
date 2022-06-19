a5a6735067d95affbdbabd782149e013dd8119b8
Francesca Hamm <francesca.c.hamm@gmail.com>
2022-06-19 00:01:05 -0500

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code here
+let createEmployeeRecord = function(employee) {
+    return {
+     firstName: employee[0],
+     familyName: employee[1],
+     title: employee[2],
+     payPerHour: employee[3],
+     timeInEvents: [],
+     timeOutEvents: []
+    }
+}
+
